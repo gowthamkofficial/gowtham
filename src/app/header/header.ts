f5c580d73171fb55d332f0f4a3eca33a2c999db5
gowthamkoffcl <gowthamkoffcl@gmail.com>
2025-06-09 21:37:12 +0530

    Designed the web view home banner

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
+  @ViewChild('navList') navList!: ElementRef;
+  @ViewChild('overlay') overlay!: ElementRef;
 
+  openSidebar() {
+    this.navList.nativeElement.classList.add('active');
+    this.overlay.nativeElement.classList.add('active');
+  }
+
+  closeSidebar() {
+    this.navList.nativeElement.classList.remove('active');
+    this.overlay.nativeElement.classList.remove('active');
+  }
 }
