83e2c3c7c15155000b983821a2d615eb32b436c6
gowthamkoffcl <gowthamkoffcl@gmail.com>
2025-06-11 21:08:29 +0530

    Completed the portfolio with responsive

--- src/app/banner/banner.ts
+++ src/app/banner/banner.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './banner.scss'
 })
 export class Banner {
-
+downloadResume() {
+  const link = document.createElement('a');
+  link.href = 'assets/resume.pdf';
+  link.download = 'GowthamK-Resume-Angular.pdf';
+  link.click();
+}
 }
